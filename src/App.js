5c99a4075cb0521522f63a5aa456b99433c37ef8
Felistus Mbesa <felistusmbesa22@gmail.com>
2023-10-30 20:32:26 +0300

    pasword generator 1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Password from './passwordGen/Password';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-row bg-slate-400">
+      <div className='w-[50%]'><Password /></div>
+      
     </div>
   );
 }
 
 export default App;
+  
---
+++ src/passGen/password.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+function Password() {
+    
+    let numbers = "1234567890";
+    let symbols = "~!@#$%^&*()_+-|{}[];:<>,./?";
+    let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    let length = 12;
+    let allChars = numbers + symbols + lowerCase + upperCase ;
+    const [newPassword, setNewPassword] = useState("");
+
+    const handleGenerator = () => {
+        let password = "";
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += numbers[Math.floor(Math.random() * numbers.length)];
+        password += symbols[Math.floor(Math.random() * symbols.length)];
+
+        while(password.length < length){
+            password += allChars[Math.floor(Math.random() * allChars.length)];
+        }
+        setNewPassword(password);
+        //passwordBox.value = password;
+        
+    }
+
+  return (
+    <div className='m-0 p-2 text-xl flex justify-center flex-col items-center bg-teal-900 text-gray-100 leading-10'>
+      <h2> Password Generator</h2>
+      <div className=''>
+        <input type="text" placeholder="password" id="passwordBox" readOnly value={newPassword} />
+        
+      </div>
+      <div className='border mt-2 bg-slate-100 text-teal-900 rounded-md'>
+        <button onClick={handleGenerator}>Generate Password</button>
+      </div>
+    </div>
+  )
+}
+
+export default Password
